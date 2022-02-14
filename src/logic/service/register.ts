187dcb7c62d83a3758711bf96271e35680b4fb30
Richard Meissner <msc.meissner@gmail.com>
2022-02-14 03:05:09 +0100

    Add more service urls

--- src/logic/service/register.ts
+++ src/logic/service/register.ts
@@ -1,6 +1,13 @@
 export const availableServices: Record<string, string> = {
     "1": "https://safe-transaction.mainnet.gnosis.io",
-    "4": "https://safe-transaction.rinkeby.gnosis.io"
+    "4": "https://safe-transaction.rinkeby.gnosis.io",
+    "5": "https://safe-transaction.goerli.gnosis.io",
+    "10": "https://safe-transaction.optimism.gnosis.io",
+    "100": "https://safe-transaction.xdai.gnosis.io",
+    "137": "https://safe-transaction.polygon.gnosis.io",
+    "42161": "https://safe-transaction.arbitrum.gnosis.io",
+    "43114": "https://safe-transaction.avalanche.gnosis.io",
+    "1313161554": "https://safe-transaction.aurora.gnosis.io"
 }
 
 export const getServiceForChainId = (chainId: string | number): string => {
